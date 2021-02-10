232e7857fc5af90acbcd3ddcb1bf29fa60c38f98
hayapo <keep.forward1001@gmail.com>
2021-02-11 01:36:20 +0900

    :zap:00しかやってないよん

--- target/debug/deps/nlp_rust-202a391039468ac0.d
+++
@@ -1,5 +0,0 @@
-/home/hayapo/nlp_rust/target/debug/deps/nlp_rust-202a391039468ac0: src/main.rs
-
-/home/hayapo/nlp_rust/target/debug/deps/nlp_rust-202a391039468ac0.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/nlp_rust-f52e53797cbca8d8.d
+++
@@ -1,5 +0,0 @@
-/home/hayapo/nlp_rust/target/rls/debug/deps/nlp_rust-f52e53797cbca8d8.rmeta: src/main.rs
-
-/home/hayapo/nlp_rust/target/rls/debug/deps/nlp_rust-f52e53797cbca8d8.d: src/main.rs
-
-src/main.rs:
